7db55cce977698ebc0eba3e8985e4bbf00a36826
Praggz007 <mraj7444@gmail.com>
2026-02-10 20:26:40 +0530

    hello

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import * as Sentry from '@sentry/nextjs';
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export function generateMetadata(): Metadata {
-  return {
-    title: "Hideout",
-    description: "Modern & Minimal JS Mastery Portfolio",
-    other: {
-      ...Sentry.getTraceData(),
-    },
-  };
-}
+export const metadata: Metadata = {
+  title: "Adrian's Portfolio",
+  description: "Modern & Minimal JS Mastery Portfolio",
+};
 
 export default function RootLayout({
   children,
@@ -25,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/pan.svg" sizes="any" />
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
